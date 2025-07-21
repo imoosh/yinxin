b1dbd3670dbf7db12e6ced539f853722a30a2a17
moosh <thekernel@163.com>
2025-07-21 10:48:12 +0800

    add install.sh

---
+++ third_deps/artifact/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# install nanomq/mbedtls/openvpn
+
+set -e
+
+declare -xr CWD=$(cd $(dirname $0); pwd)
+
+# install dir
+declare -xr DIR=/usr/local
+
+tar -xf "$CWD"/nanomq-0.23.10.tar.gz -C "$DIR" --strip-components=1
+tar -xf "$CWD"/openvpn-v2.6.14.tar.gz -C "$DIR" --strip-components=1
+tar -xf "$CWD"/mbedtls-v2.28.10.tar.gz -C "$DIR" --strip-components=1
+
+echo "/usr/local/lib64" > /etc/ld.so.conf.d/nanomq.conf
+ldconfig
+
+ln -sf /usr/local/etc/nanomq_old.conf /etc/nanomq_old.conf
