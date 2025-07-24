3de39ec168a948ce337a7ce66269585d7db3c7ff
lining <lining@example.com>
2025-07-24 23:48:27 +0800

    删除冗余的cs_src ,只保留最外层的tests/c_http_server/cs_src

--- src/iotvpn_config_manager_plugin/nanomq/tests/cs_src/env.sh
+++
@@ -1,4 +0,0 @@
-# source this scirpt
-# usage: srouce ./env.sh
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../../build_tmp
--- src/印信demo/cs_src/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#curl -X POST -H "Content-Type: application/json" \
-# -d '{"username":    "cs","password": "Cs@1234"}' http://127.0.0.1:8000/login -v
-
-curl -k http://127.0.0.1:8000/test -v
-#curl --http1.1 -X put http://127.0.0.1:8000/test -v
-#curl --http1.1 -k http://127.0.0.1:8000/test -v 
